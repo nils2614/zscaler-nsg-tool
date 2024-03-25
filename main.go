e2d5322e6989050669ff8567c6966c245b9b7f9e
nils2614 <63887691+nils2614@users.noreply.github.com>
2024-03-25 15:45:47 +0100

    Add struct definitions for data received from API

--- main.go
+++ main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+// Struct to hold information from Zscaler zpa api
+type zpaApi struct {
+	CloudName string `json:"Cloud Name"`
+	Content   []struct {
+		IPProtocol string   `json:"IP Protocol"`
+		Port       int      `json:"Port"`
+		Source     string   `json:"Source"`
+		Domains    string   `json:"Domains"`
+		IPs        []string `json:"IPs"`
+		DateAdded  string   `json:"Date Added"`
+	} `json:"content"`
+}
+
+type hubApi struct {
+	CloudName   string   `json:"cloudName"`
+	Type        string   `json:"type"`
+	HubPrefixes []string `json:"hubPrefixes"`
+}
+
 func main() {
 	fmt.Println("Hello World!")
 }
