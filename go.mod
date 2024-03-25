696145b2b147686895cf7d172e8a651009a49b2d
nils2614 <63887691+nils2614@users.noreply.github.com>
2024-03-25 17:09:42 +0100

    Add initial support for parsing config file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module zscaler-nsg-tool
 
 go 1.22
+
+require github.com/pelletier/go-toml/v2 v2.2.0
--- main.go
+++ main.go
@@ -1,26 +1,84 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pelletier/go-toml/v2"
+	"os"
+)
 
-// Struct to hold information from Zscaler zpa api
-type zpaApi struct {
-	CloudName string `json:"Cloud Name"`
-	Content   []struct {
-		IPProtocol string   `json:"IP Protocol"`
-		Port       int      `json:"Port"`
-		Source     string   `json:"Source"`
-		Domains    string   `json:"Domains"`
-		IPs        []string `json:"IPs"`
-		DateAdded  string   `json:"Date Added"`
-	} `json:"content"`
-}
+// Struct to hold information from Zscaler API and config file
+type (
+	zpaApi struct {
+		CloudName string `json:"Cloud Name"`
+		Content   []struct {
+			IPProtocol string   `json:"IP Protocol"`
+			Port       int      `json:"Port"`
+			Source     string   `json:"Source"`
+			Domains    string   `json:"Domains"`
+			IPs        []string `json:"IPs"`
+			DateAdded  string   `json:"Date Added"`
+		} `json:"content"`
+	}
 
-type hubApi struct {
-	CloudName   string   `json:"cloudName"`
-	Type        string   `json:"type"`
-	HubPrefixes []string `json:"hubPrefixes"`
-}
+	hubApi struct {
+		CloudName   string   `json:"cloudName"`
+		Type        string   `json:"type"`
+		HubPrefixes []string `json:"hubPrefixes"`
+	}
+
+	myConfig struct {
+		Main      cfgMain
+		Zscaler   cfgZscaler
+		Resources cfgResources
+	}
+
+	cfgMain struct {
+		Priority int
+		Output   string
+	}
+
+	cfgZscaler struct {
+		Zpa    cfgZscalerZpa
+		Hub    cfgZscalerHub
+		Custom cfgZscalerCustom
+	}
+
+	cfgZscalerZpa struct {
+		Enabled bool
+		Url     string
+	}
+
+	cfgZscalerHub struct {
+		Enabled bool
+		Url     string
+	}
+
+	cfgZscalerCustom struct {
+		Enabled bool
+		Url     string
+	}
+
+	cfgResources struct {
+		RgNameTf  string
+		NsgNameTf string
+		NsgNameAz string
+	}
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Starting..")
+
+	var cfg myConfig
+	configFile, err := os.ReadFile("config.toml")
+	if err != nil {
+		fmt.Println("Error loading config file:", err.Error())
+	} else {
+		err = toml.Unmarshal(configFile, &cfg)
+		if err != nil {
+			fmt.Println("Error loading config file:", err.Error())
+		}
+	}
+
+	// Test
+	fmt.Println(cfg.Zscaler.Custom.Enabled)
 }
